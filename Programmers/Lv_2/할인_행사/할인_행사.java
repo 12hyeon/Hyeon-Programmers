56c9970ba6483f58c3894d51b16163948c2a7906
12hyeon <hyeonjeong1212@naver.com>
2024-01-24 14:27:22 +0900

    feat: 할인행사 java

---
+++ Programmers/Lv_2/할인_행사/할인_행사.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Solution {
+    public int solution(String[] want, int[] number, String[] discount) {
+        int answer = 0;
+
+        Map<String, Integer> w = new HashMap<String, Integer>();
+        for (int i=0; i<number.length; i++) {
+            w.put(want[i], number[i]);
+        }
+        // 문자열 : substring(), split()
+        // 문자열 리스트 :Arrays.copyOfRange(arr, 0, position);
+        // List -> [] : a.toArray(new String[size]);
+        // Array [] -> List : Arrays.asList();
+        List<String> d = Arrays.asList(discount);
+
+        // String[] 도 length
+        for (int i=0; i<discount.length-9; i++) {
+            // List 일부 : subList()
+            List<String> de = d.subList(i, i+10);
+
+            Map<String, Integer> cmp = new HashMap<String, Integer>(); // [] x, for문 o
+            for (String e : de) {
+                // Map : getOrDefault(get의 인자, 없을때 사용할 값)
+                cmp.put(e, cmp.getOrDefault(e,0)+1);
+            }
+
+            if (w.equals(cmp)) {
+                answer += 1;
+            }
+        }
+
+        return answer;
+    }
+}
--- Programmers/Lv_2/할인_행사/할인_행사.py
+++ Programmers/Lv_2/할인_행사/할인_행사.py
@@ -1,56 +1,14 @@
+from collections import Counter
+
+# list -> map : Counter로 각 인자의 개수
 def solution(want, number, discount):
     answer = 0
+    dic = {}
+    for i in range(len(want)):
+        dic[want[i]] = number[i] # want -> key-value
 
-    lw = len(want)
-    ld = len(discount)
-    n = len(number)
+    for i in range(len(discount)-9): # 10개 묶음 수
+        if dic == Counter(discount[i:i+10]): # 각 내용을 dict으로 생성
+            answer += 1
 
-    # 숫자로 매칭
-    pd = []
-    p = []
-    dsc = []
-    for d in discount:
-        if d not in pd:
-            pd.append(d)
-        dsc.append(pd.index(d))
-    wnt = []
-    for w in want:
-        if w not in pd:
-            pd.append(w)
-        wnt.append(pd.index(w))
-
-    # 목표 숫자로
-    goal = [0] * len(pd)
-    for i, w in enumerate(want):
-        idx = pd.index(w)
-        goal[idx] += number[i]
-
-    # print(pd, goal)
-    # print(dsc, wnt)
-
-    def cmp(a, b):  # now, goal
-        for i in range(len(a)):
-            if a[i] < b[i]:
-                return 0
-        return 1
-
-    # 첫날 결재
-    now = [0] * len(pd)
-    for i in range(min(ld, 10)):
-        now[dsc[i]] += 1
-    answer += cmp(now, goal)
-    # print(pd)
-    # print(now)
-
-    s = 0  # 미포함
-    end = 10  # 포함
-    for _ in range(ld - 10):
-        now[dsc[s]] -= 1
-        if end < ld:
-            now[dsc[end]] += 1
-        answer += cmp(now, goal)
-        # print(now, goal)
-
-        s += 1
-        end += 1
     return answer
