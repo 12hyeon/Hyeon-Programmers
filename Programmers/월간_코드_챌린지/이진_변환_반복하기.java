43a58256ddbe44a09f18d7efdaf6d85ed8404dde
12hyeon <hyeonjeong1212@naver.com>
2024-06-16 17:26:12 +0900

    feat: 이진 변환 반복하기 java

---
+++ Programmers/월간_코드_챌린지/이진_변환_반복하기.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+
+    public int[] answer = new int[2];
+
+    private String check(String s) {
+        char [] a = s.toCharArray();
+        int cnt = 0;
+        for(int i : a) {
+            if (i == '1') cnt += 1;
+            else answer[1] += 1;
+        }
+        return Integer.toBinaryString(cnt);
+    }
+
+    public int[] solution(String s) {
+        answer[0] = 1;
+        String std = s;
+        while (true) {
+            String v = check(std);
+            System.out.println(v);
+            if (v.equals("1")) {
+                break;
+            } else {
+                std = v;
+            }
+            answer[0] += 1;
+        }
+
+        return answer;
+    }
+}
